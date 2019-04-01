aa4b37557822aa03fa581ea7be5a6c0f6fe932ac
Mike Joyce <mikejoyce19@gmail.com>
2019-03-31 21:53:54 -0500

    made change

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import './styles.css'
 
 const list = document.getElementById('messages')
 
-document.addEventListener('message', evt => {
-  console.log(evt)
-  const li = document.createElement('li')
-  li.innerText = evt.data.toString()
-  list.appendChild(li)
-})
+document.addEventListener(
+  'message',
+  evt => {
+    console.log(evt)
+    const li = document.createElement('li')
+    li.innerText = JSON.stringify(evt.data)
+    list.appendChild(li)
+  },
+  false
+)
